6182bc100da5d0c560c85793f727214d703da9d3
kalasni <kalassni@gmail.com>
2015-09-15 21:00:46 +0200

    Added contact about pages and header and footer files

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 
+gem 'bootstrap-sass'
+
 # Use SCSS for stylesheets
 gem 'sass-rails'
 # Use Uglifier as compressor for JavaScript assets
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,7 +15,6 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     #assert_select "title", "Home | #{@base_title}"
     assert_select "title", "Ruby on Rails Tutorial Sample App"
-
   end
 
   test "should get help" do
@@ -23,7 +22,6 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     #assert_select "title", "Help | #{@base_title}"
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-
   end
 
   test "should get about" do
@@ -32,5 +30,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     #assert_select "title", "About | #{@base_title}"
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 
 end
