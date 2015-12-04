33f53ad855c8447d8f707eaa5354cff9996a7915
kalasni <kalassni@gmail.com>
2015-12-04 19:15:44 +0100

    Add passwords resets. Email hadn't been configured, chapter 10.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
 =begin
 
   root 'static_pages#home'
@@ -20,6 +24,8 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,  only: [:new, :create, :edit, :update]
+
 
 
 
